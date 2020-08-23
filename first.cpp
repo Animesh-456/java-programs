d0595955b01b65ef3668a4415a029bf125cdaa0b
Animesh Mondal <Anim@DESKTOP-PHHCHSR.localdomain>
2020-08-23 17:28:20 +0530

    My first c++ program

---
+++ first.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a, b,s;
+	cout << "Enter the first number :\n";
+	cin >> a;
+	cout << "enter te second number :\n" ;
+	cin >> b;
+	s=a+b;
+	cout << "the sum of the numbers is :" << s;
+	return 0;
+}
